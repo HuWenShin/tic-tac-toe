4c7fb918f6392ccfe61de8347b1dc7f8d08bdcca
Ruby Hu <60735962+HuWenShin@users.noreply.github.com>
2024-07-14 19:07:54 -0700

    Work out logic and simple styling
    Worked out the logic of the game and styled the game with simple css

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-function gameboard() {
-    const rows = 3;
-    const columns = 3;
-    const board = [];
+const gameBoard = (function() {
 
-    for (let i = 0; i < rows; i ++) {
-            board[i] = [];
-            for (let j = 0; j < columns; j ++) {
-                board[i].push(cell());
-        }
+    const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    const placeToken = (token, index) => {
+        if (index > board.length) return;
+        board[index] = token;
     }
 
     const getBoard = () => board;
 
+    const getCell = (index) => {
+        return board[index];
+    }
 
-    const placeToken = (row, column, player) => {
-        if (board[row][column] === 0) {
-            board[row][column] = player;
-        } else {
-            return
+    const resetBoard = () => {
+        for (let i = 0; i < board.length; i ++) {
+            board[i] = 0;
         }
     }
 
@@ -25,27 +23,131 @@ function gameboard() {
         console.log(board);
     }
 
-    return { getBoard, placeToken, printBoard };
-}
+    return { placeToken, getBoard, getCell, resetBoard, printBoard };
 
+})();
 
-//control cell operations
-function cell() {
-    let value = 0;
+const player = (sign) => {
 
-    const addToken = (player) => {
-        value = player;
+    const getSign = () => {
+        return sign;
     }
 
-    const getValue = () => value;
-
-    return { addToken, getValue };
+    return { getSign };
 }
 
+const displayManager = (function() {
+    const displayMessage = document.getElementById("display-message");
+    // const boardArea = document.getElementsByClassName("board");
+    const cells = document.querySelectorAll(".cell");
+    const restartButton = document.getElementById("restart-btn");
 
-function gameController() {
-    const playerOne = 1;
-    const playerTwo = 2;
+    const setMessage = (message) => {
+        displayMessage.textContent = message;
+    }
+
+    const updateBoard = () => {
+        for (let i = 0; i < cells.length; i ++) {
+            if (gameBoard.getCell(i) == 1) {
+                cells[i].innerText = "X";
+            } else if (gameBoard.getCell(i) == 2) {
+                cells[i].innerText = "O";
+            } else {
+                cells[i].innerText = "";
+            }
+        }
+    }
+
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseover", (e) => {
+            if (e.target.innerText == "") {
+                e.target.style.cursor = "pointer";
+            } else {
+                e.target.style.cursor = "default";
+            }
+        });
+    
+        cell.addEventListener("mouseout", (e) => {
+            e.target.style.cursor = "default"; // Reset the cursor style
+        });
+        
+        cell.addEventListener("click", (e) => {
+            if (gameController.gameOver() || e.target.innerText !== "") return;
+            gameController.playRound(parseInt(e.target.dataset.index));
+            updateBoard();
+        })
+    });
+
+    // when restart button clicked, clear board for new game
+    restartButton.addEventListener("click", () => {
+        gameController.resetGame();
+        gameBoard.resetBoard();
+        updateBoard();
+        setMessage("Click any cell to start game.");
+    });
+
+    return { setMessage }
+})();
 
 
-}
+const gameController = (function () {
+    const player1 = player(1);
+    const player2 = player(2);
+    let round = 1;
+    let isGameOver = false;
+
+    const currentPlayer = () => {
+        return round % 2 == 1 ? player1.getSign() : player2.getSign();
+    }
+
+    const playRound = (cellIndex) => {
+        gameBoard.placeToken(currentPlayer(), cellIndex);
+        
+        if (checkWin(cellIndex)) {
+            // print win message
+            displayManager.setMessage(`Player ${currentPlayer()} won.`);
+            isGameOver = true;
+            return;
+        }
+
+        if (round === 9) {
+            // annouce draw
+            displayManager.setMessage("Draw, try again.");
+            isGameOver = true;
+            return;
+        }
+        round ++;
+
+        // set message for next player's turn
+        displayManager.setMessage(`Player ${currentPlayer()}'s turn.`);
+        
+    }
+
+    const checkWin = (cellIndex) => {
+        const winCondition = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        return winCondition
+            .filter((potentialWin) => potentialWin.includes(cellIndex))
+            .some((winCombos) => winCombos.every((index) => gameBoard.getCell(index) == currentPlayer()));
+    }
+
+    const gameOver = () => {
+        return isGameOver;
+    }
+
+    const resetGame = () => {
+        round = 1;
+        isGameOver = false;
+    }
+
+    return { currentPlayer, playRound, resetGame, gameOver }
+})();
